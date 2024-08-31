25429568261803a319acad2f9993c30077de6077
Ijeoma Jahsway <uchennamebijay@gmail.com>
2024-08-31 15:03:41 +0100

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import FuturisticMarkdownPreviewer from './components/FuturisticMarkdownPreviewer';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="App">
+      <FuturisticMarkdownPreviewer />
+    </div>
+    </>
+  )
+}
+
+export default App
